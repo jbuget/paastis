8f515868d2b3f8564447c46668bf3582a2ffb9d6
jbuget <jbuget@gmail.com>
2022-09-09 08:49:14 +0200

    fix: yet another try to fix Scalingo start

--- paastis-proxy/config.js
+++ paastis-proxy/config.js
@@ -12,7 +12,7 @@ function parseIgnoredApps() {
 
 const config = {
   server: {
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || '0.0.0.0',
     port: parseInt(process.env.PORT, 10) || 3000,
   },
   registry: {
