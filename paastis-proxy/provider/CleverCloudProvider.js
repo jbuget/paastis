aae65225a09f3bfc2294c790a2e7f8aca2381b40
jbuget <jbuget@gmail.com>
2022-09-10 20:13:03 +0200

    doc: business model + CleverCloud

--- paastis-proxy/provider/CleverCloudProvider.js
+++ paastis-proxy/provider/CleverCloudProvider.js
@@ -2,12 +2,22 @@ import { PaasProvider } from "./PaasProvider.js";
 
 export class CleverCloudProvider extends PaasProvider {
 
+  constructor() {
+    super();
+  }
+
   async listAllApps() {
 
   }
 
   async getClient(region) {
-
+    const API_HOST = 'https://api.clever-cloud.com'
+    const tokens = {
+      OAUTH_CONSUMER_KEY: 'your OAUTH_CONSUMER_KEY',
+      OAUTH_CONSUMER_SECRET: 'your OAUTH_CONSUMER_SECRET',
+      API_OAUTH_TOKEN: 'your API_OAUTH_TOKEN',
+      API_OAUTH_TOKEN_SECRET: 'your API_OAUTH_TOKEN_SECRET',
+    }
   }
 
   async isAppRunning(appId, region) {
